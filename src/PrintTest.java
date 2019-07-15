42f3824f3ce9ba8b88d957e171d7366fa6fabd9c
Danny <2239337385@qq.com>
2019-07-15 09:00:17 +0800

    Git Test

---
+++ src/PrintTest.java
@@ -0,0 +1,9 @@
+import static net.mindview.util.print.*;
+public class PrintTest {
+    public static void main(String[] args){
+        print("Available from now on!");
+        print(100);
+        print(100L);
+        print(3.14159);
+    }
+}
