1814c5c3f745567d73f5c38cefbdddd8f99c77a6
Danny <2239337385@qq.com>
2019-10-15 08:35:52 +0800

    Git Test

---
+++ src/polymorphism/shape/Circle.java
@@ -0,0 +1,7 @@
+package polymorphism.shape;
+import static net.mindview.util.print.*;
+
+public class Circle extends Shape{
+    public void draw(){print("Circle.draw()");}
+    public void erase(){print("Circle.erase()");}
+}
---
+++ src/polymorphism/shape/Square.java
@@ -0,0 +1,8 @@
+package polymorphism.shape;
+
+import static net.mindview.util.print.*;
+
+public class Square extends Shape{
+    public void draw(){print("Square.draw()");}
+    public void erase(){print("Square.erase()");}
+}
